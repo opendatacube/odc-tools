57f9027a8b8f67cca77c1d04c749fc031a8ece8f
Kirill Kouzoubov <kirill888@gmail.com>
2018-09-05 10:24:26 +1000

    Add native tiling to dstiler (Landsat only for now)

--- dea/apps/dstiler.py
+++ dea/apps/dstiler.py
@@ -1,6 +1,6 @@
 import click
 import dscache
-from .tiling import bin_dataset_stream
+from .tiling import bin_dataset_stream, bin_by_native_tile
 from datacube.model import GridSpec
 import datacube.utils.geometry as geom
 
@@ -11,17 +11,31 @@ GS_ALBERS = GridSpec(crs=geom.CRS('EPSG:3577'),
 
 
 @click.command('dstiler')
+@click.option('--native', is_flag=True, help='Use Landsat Path/Row as grouping')
 @click.argument('dbfile', type=str, nargs=1)
-def cli(dbfile):
+def cli(native, dbfile):
+    """Add spatial grouping to file db.
+
+    Default grid is Australian Albers (EPSG:3577) with 100k by 100k tiles. But
+    you can also group by Landsat path/row.
+
+    """
     cache = dscache.open_rw(dbfile)
     label = 'Processing {} ({:,d} datasets)'.format(dbfile, cache.count)
 
-    gs = GS_ALBERS  # TODO: make configurable
+    if native:
+        gs = None
+        group_key_fmt = 'native/{:03d}{:03d}'
+    else:
+        gs = GS_ALBERS  # TODO: make configurable
+        group_key_fmt = 'albers/{:+03d}{:+03d}'
 
     with click.progressbar(cache.get_all(), length=cache.count, label=label) as dss:
-        bins = bin_dataset_stream(gs, dss)
+        if native:
+            bins = bin_by_native_tile(dss)
+        else:
+            bins = bin_dataset_stream(gs, dss)
 
-    group_key_fmt = 'albers/{:+03d}{:+03d}'
     click.echo('Total bins: {:d}'.format(len(bins)))
 
     with click.progressbar(bins.values(), length=len(bins), label='Saving') as groups:
--- dea/dscache/tools/tiling.py
+++ dea/dscache/tools/tiling.py
@@ -1,6 +1,13 @@
 from types import SimpleNamespace
 
 
+def extract_ls_path_row(ds):
+    full_id = ds.metadata_doc.get('tile_id')
+    if full_id is None:
+        return None
+    return tuple(int(s) for s in (full_id[3:6], full_id[6:9]))
+
+
 def bin_dataset_stream(gridspec, dss, persist=None):
     """
 
@@ -29,5 +36,44 @@ def bin_dataset_stream(gridspec, dss, persist=None):
 
         for tile, geobox in gridspec.tiles_from_geopolygon(ds.extent, geobox_cache=geobox_cache):
             register(tile, geobox, ds_val)
+
+    return cells
+
+
+def bin_by_native_tile(dss, persist=None, native_tile_id=None):
+    """Group datasets by native tiling, like path/row for Landsat.
+
+    :param dss: Sequence of datasets (can be lazy)
+
+    :param persist: Dataset -> SomeThing mapping, defaults to keeping dataset
+    id only
+
+    :param native_tile_id: Dataset -> Key, defaults to extracting `path,row`
+    tuple from metadata's `tile_id` field, but could be anything, only
+    constraint is that Key value can be used as index to python dict.
+    """
+
+    cells = {}
+
+    def default_persist(ds):
+        return ds.id
+
+    def register(tile, val):
+        cell = cells.get(tile)
+        if cell is None:
+            cells[tile] = SimpleNamespace(idx=tile, dss=[val])
+        else:
+            cell.dss.append(val)
+
+    native_tile_id = native_tile_id or extract_ls_path_row
+    persist = persist or default_persist
+
+    for ds in dss:
+        ds_val = persist(ds)
+        tile = native_tile_id(ds)
+        if tile is None:
+            raise ValueError('Missing tile id')
+        else:
+            register(tile, ds_val)
 
     return cells
