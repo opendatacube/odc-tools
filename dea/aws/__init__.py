f6c7973c5f9941127c80114de25b7084ac819fe4
Kirill Kouzoubov <kirill888@gmail.com>
2018-10-16 08:51:56 +1100

    Credentials refresh in async code
    Dealing with sts credentials for long running tasks

--- dea/aws/__init__.py
+++ dea/aws/__init__.py
@@ -2,6 +2,9 @@ import requests
 from urllib.parse import urlparse
 import botocore
 import botocore.session
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def ec2_metadata(timeout=0.1):
@@ -152,10 +155,29 @@ def s3_get_object_request_maker(region_name=None, credentials=None, ssl=True):
         region_name = auto_find_region()
 
     if credentials is None:
-        credentials = session.get_credentials().get_frozen_credentials()
+        managed_credentials = session.get_credentials()
+        credentials = managed_credentials.get_frozen_credentials()
+    else:
+        managed_credentials = None
 
     protocol = 'https' if ssl else 'http'
     auth = S3SigV4Auth(credentials, 's3', region_name)
+
+    def maybe_refresh_credentials():
+        nonlocal credentials
+        nonlocal auth
+
+        if not managed_credentials:
+            return
+
+        creds = managed_credentials.get_frozen_credentials()
+        if creds is credentials:
+            return
+
+        log.debug("Refreshed credentials (s3_get_object_request_maker)")
+
+        credentials = creds
+        auth = S3SigV4Auth(credentials, 's3', region_name)
 
     def build_request(bucket=None,
                       key=None,
@@ -172,6 +194,8 @@ def s3_get_object_request_maker(region_name=None, credentials=None, ssl=True):
 
         if isinstance(Range, (tuple, list)):
             Range = 'bytes={}-{}'.format(Range[0], Range[1]-1)
+
+        maybe_refresh_credentials()
 
         headers = {}
         if Range is not None:
