2959c9dd814554ab7a66f2e5c0c138530ab2c833
Kirill Kouzoubov <kirill888@gmail.com>
2018-10-24 11:27:55 +1100

    Better keeping track of errors
    previously would only report failure to index, now counts failure to load also.

--- dea/apps/index_from_tar.py
+++ dea/apps/index_from_tar.py
@@ -1,4 +1,5 @@
 import click
+import sys
 import datacube
 from datacube.index.hl import Doc2Dataset
 from dea.io import tar_doc_stream
@@ -6,26 +7,30 @@ from dea.io.text import parse_yaml
 
 
 def from_tar_file(tarfname, index, mk_uri, **kwargs):
+    """ returns a sequence of tuples where each tuple is either
+
+        (ds, None) or (None, error_message)
+    """
     doc2ds = Doc2Dataset(index, **kwargs)
 
     for doc_name, doc in tar_doc_stream(tarfname):
         try:
             metadata = parse_yaml(doc)
         except Exception as e:
-            print('Error: %s, %s' % (doc_name, str(e)))
+            yield (None, 'Error: %s, %s' % (doc_name, str(e)))
             continue
 
         if metadata is None:
-            print('Error: failed to parse: %s, "%s"' % (doc_name, doc))
+            yield (None, 'Error: failed to parse: %s, "%s"' % (doc_name, doc))
             continue
 
         uri = mk_uri(doc_name)
 
         ds, err = doc2ds(metadata, uri)
         if ds is not None:
-            yield ds
+            yield (ds, None)
         else:
-            print('Error: %s, %s' % (doc_name, err))
+            yield (None, 'Error: %s, %s' % (doc_name, err))
 
 
 @click.command('index_from_tar')
@@ -36,17 +41,25 @@ def cli(input_fname, env=None):
     def mk_s3_uri(name):
         return 's3://' + name
 
+    def report_error(msg):
+        print(msg, file=sys.stderr)
+
     def process_file(filename, index):
         n_total = 0
         n_failed = 0
 
-        for ds in from_tar_file(filename, index, mk_s3_uri, verify_lineage=False):
+        for ds, err in from_tar_file(filename, index, mk_s3_uri, verify_lineage=False):
             n_total += 1
-            try:
-                index.datasets.add(ds, with_lineage=True)
-            except Exception as e:
+
+            if ds is not None:
+                try:
+                    index.datasets.add(ds, with_lineage=True)
+                except Exception as e:
+                    n_failed += 1
+                    report_error(str(e))
+            else:
                 n_failed += 1
-                print(str(e))
+                report_error(err)
 
             if (n_total % 10) == 0:
                 print('.', end='', flush=True)
