305dae74ba4b5454362ee80da968ed34f473002f
Kirill Kouzoubov <kirill888@gmail.com>
2018-10-25 12:57:59 +1100

    Add path normalisation utility

--- dea/io/__init__.py
+++ dea/io/__init__.py
@@ -2,5 +2,6 @@
 """
 from .text import parse_yaml, read_stdin_lines, slurp_lines
 from .tar import tar_doc_stream
+from .path import default_base_dir, normalise_path
 
 __all__ = ['parse_yaml', 'read_stdin_lines', 'slurp_lines', 'tar_doc_stream']
