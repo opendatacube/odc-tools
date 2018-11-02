5db656f36126ff3a39395ad41c52edfbff27c80f
Kirill Kouzoubov <kirill888@gmail.com>
2018-11-02 14:04:14 +1100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dea-proto',
-    version='0.1',
+    version='0.2',
     license='Apache License 2.0',
     packages=find_packages(),
     author='Kirill Kouzoubov',
