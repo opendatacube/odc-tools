757c53cc6b379d14e8f586513bf09d487f1c38c2
Kirill Kouzoubov <kirill888@gmail.com>
2018-11-16 11:26:05 +1100

    Support non-glob queries better
    When looking for specific filename at a fixed depth use head_object instead of
    list_objects to verify file existence, also have an option to skip checking that
    file exists and just assume it's there.
    
    Also support listing "directories" only.

--- dea/apps/s3_find.py
+++ dea/apps/s3_find.py
@@ -1,12 +1,16 @@
 import click
 import sys
+from types import SimpleNamespace
 from dea.aws.aio import S3Fetcher
 from dea.aws._find import parse_query, norm_predicate
+from dea.ppr import future_results
 
 
 @click.command('s3-find')
+@click.option('--skip-check', is_flag=True,
+              help='Assume file exists when listing exact file rather than wildcard.')
 @click.argument('uri', type=str, nargs=1)
-def cli(uri):
+def cli(uri, skip_check):
     """ List files on S3 bucket.
 
     Example:
@@ -22,14 +26,40 @@ def cli(uri):
        \b
        List files that match `*yaml` 2 levels deep from known path
         > s3-find 's3://mybucket/some/path/*/*/*yaml'
+
+       \b
+       List directories 2 levels deep from known path
+        > s3-find 's3://mybucket/some/path/*/*/'
+
+       \b
+       List all files named `metadata.yaml` 2 directories deep
+        > s3-find 's3://mybucket/some/path/*/*/metadata.yaml'
     """
 
-    def do_query(qq, pred):
+    def do_file_query(qq, pred):
         for d in s3.dir_dir(qq.base, qq.depth):
             _, _files = s3.list_dir(d).result()
             for f in _files:
                 if pred(f):
                     yield f
+
+    def do_file_query2(qq):
+        fname = qq.file
+
+        stream = s3.dir_dir(qq.base, qq.depth)
+
+        if skip_check:
+            yield from (SimpleNamespace(url=d+fname) for d in stream)
+            return
+
+        stream = (s3.head_object(d+fname) for d in stream)
+
+        for (f, _), _ in future_results(stream, 32):
+            if f is not None:
+                yield f
+
+    def do_dir_query(qq):
+        return (SimpleNamespace(url=url) for url in s3.dir_dir(qq.base, qq.depth))
 
     flush_freq = 100
 
@@ -41,14 +71,19 @@ def cli(uri):
 
     s3 = S3Fetcher()
 
-    glob = qq.glob or qq.file
+    glob_or_file = qq.glob or qq.file
 
-    if qq.depth < 0 or (qq.glob is None and qq.file is None):
-        stream = s3.find(qq.base, glob=glob)
+    if (qq.depth is None and glob_or_file is None) or qq.depth < 0:
+        stream = s3.find(qq.base, glob=glob_or_file)
     else:
         # fixed depth query
-        pred = norm_predicate(glob=glob)
-        stream = do_query(qq, pred)
+        if qq.glob is not None:
+            pred = norm_predicate(glob=qq.glob)
+            stream = do_file_query(qq, pred)
+        elif qq.file is not None:
+            stream = do_file_query2(qq)
+        else:
+            stream = do_dir_query(qq)
 
     for i, o in enumerate(stream):
         print(o.url, flush=(i % flush_freq == 0))
