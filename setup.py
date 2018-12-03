91bbda92c05f5b1d805bc03877b4c7963ee31369
Kirill Kouzoubov <kirill888@gmail.com>
2018-12-03 11:25:41 +1100

    Remove no longer used directly aiohttp dependency

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ setup(
                       'toolz',
                       'zstandard',
                       'lmdb',
-                      'aiohttp',
                       'aiobotocore',
                       'botocore',
                       ],
