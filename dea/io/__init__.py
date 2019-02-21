b97b890a12d854b2a7201c710a9c2dc94b92a9d4
Kirill Kouzoubov <kirill888@gmail.com>
2019-02-22 09:50:35 +1100

    Update __all__ in io module
    was missing some imported functions

--- dea/io/__init__.py
+++ dea/io/__init__.py
@@ -4,4 +4,11 @@ from .text import parse_yaml, read_stdin_lines, slurp_lines
 from .tar import tar_doc_stream
 from .path import default_base_dir, normalise_path
 
-__all__ = ['parse_yaml', 'read_stdin_lines', 'slurp_lines', 'tar_doc_stream']
+__all__ = (
+    'parse_yaml',
+    'read_stdin_lines',
+    'slurp_lines',
+    'tar_doc_stream',
+    'default_base_dir',
+    'normalise_path',
+)
