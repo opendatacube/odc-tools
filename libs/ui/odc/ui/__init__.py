a0d52438c7a10934b7666320929748fe26b8138d
Kirill Kouzoubov <kirill888@gmail.com>
2019-04-29 17:05:18 +1000

    Add ui_poll helper method
    Allows to block ipython execution of cells while still processing ui events

--- libs/ui/odc/ui/__init__.py
+++ libs/ui/odc/ui/__init__.py
@@ -52,6 +52,61 @@ def simple_progress_cbk(n, total):
     print('\r{:4d} of {:4d}'.format(n, total), end='', flush=True)
 
 
+def _ui_poll(f, sleep):
+    from IPython import get_ipython
+    import time
+    import sys
+
+    shell = get_ipython()
+    kernel = shell.kernel
+    events = []
+    kernel.shell_handlers['execute_request'] = lambda *e: events.append(e)
+    current_parent = (kernel._parent_ident, kernel._parent_header)
+    shell.execution_count += 1
+
+    def replay_events(shell, events):
+        kernel = shell.kernel
+        sys.stdout.flush()
+        sys.stderr.flush()
+        for stream, ident, parent in events:
+            kernel.set_parent(ident, parent)
+            kernel.execute_request(stream, ident, parent)
+            if shell._last_traceback is not None:
+                # there was an exception drop rest on the floor
+                break
+
+    def finalise():
+        replay_events(shell, events)
+
+    try:
+        x = f()
+        while x is None:
+            kernel.do_one_iteration()
+            kernel.set_parent(*current_parent)  # ensure stdout still happens in the same cell
+            if sleep is not None:
+                time.sleep(sleep)
+            x = f()
+        return x, finalise
+    except Exception as e:
+        raise e
+    finally:
+        kernel.shell_handlers['execute_request'] = kernel.execute_request
+
+
+def ui_poll(f, sleep=0.02):
+    import asyncio
+    from warnings import warn
+
+    x, finalise = _ui_poll(f, sleep)
+    loop = asyncio.get_event_loop()
+    if loop.is_running():
+        loop.call_soon(finalise)
+    else:
+        warn('Automatic execution of scheduled cells only works with asyncio based ipython')
+
+    return x
+
+
 def show_datasets(dss):
     from IPython.display import GeoJSON
     from datacube.testutils.geom import epsg4326
