a96d21612f831e81744aaf6273b96c5367df90ce
Kirill Kouzoubov <kirill888@gmail.com>
2019-06-05 17:52:10 +1000

    Add dev_install.sh helper script

---
+++ scripts/dev_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# order is important: need to install dependencies first
+# leaf nodes: io aws ppt geom dscache
+# nodes with odc.* dependencies:
+# ------------------------------
+#   dtools <: aws
+#   index  <: io
+#   ui     <: index
+#   aio    <: aws ppt     # skipping aio install, has hard botocore constraints
+
+LIBS="io aws ppt geom dscache"
+LIBS+=" dtools index ui"
+
+
+for lib in $LIBS; do
+    echo "Installing odc-${lib}"
+    pip install -e libs/${lib}
+done
