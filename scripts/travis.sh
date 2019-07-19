b281029a7795e1d8fbcf8ca21b168fb4f46a8dce
Kirill Kouzoubov <kirill888@gmail.com>
2019-07-19 12:28:14 +1000

    Installing all the libs/apps on travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-echo "TODO"
-pwd
-env
+set -eux
+
+# order is important: need to install dependencies first
+# leaf nodes: io aws ppt geom dscache
+# nodes with odc.* dependencies:
+# ------------------------------
+#   dtools <: aws
+#   index  <: io
+#   ui     <: index
+#   aio    <: aws ppt
+
+LIBS="io aws ppt geom dscache"
+LIBS+=" dtools index ui"
+LIBS+=" aio"
+
+APPS="cloud dc_tools dnsup"
+
+
+for lib in $LIBS; do
+    echo "Installing odc-${lib}"
+    pip install libs/${lib}
+done
+
+for app in $APPS; do
+    echo "Installing $app"
+    pip install apps/${app}
+done
