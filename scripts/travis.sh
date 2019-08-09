deec923c3d5cd5d365f78df52f528c4f130999b4
Kirill Kouzoubov <kirill888@gmail.com>
2019-08-09 14:29:28 +1000

    Build wheels on travis

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -16,13 +16,25 @@ LIBS+=" dtools index ui"
 LIBS+=" aio"
 
 APPS="cloud dc_tools dnsup"
+PP=()
+WHEEL_DIR=$(pwd)/wheels
+mkdir -p "${WHEEL_DIR}"
 
 for lib in $LIBS; do
     echo "Installing odc-${lib}"
     (cd libs/${lib} && python setup.py install)
+    PP+=(libs/${lib})
 done
 
 for app in $APPS; do
     echo "Installing $app"
     (cd apps/${app} && python setup.py install)
+    PP+=(apps/${app})
 done
+
+for p in "${PP[@]}"; do
+    (cd "${p}" &&
+     python setup.py bdist_wheel --dist-dir "${WHEEL_DIR}")
+done
+
+echo "Wheels are in: ${WHEEL_DIR}"
