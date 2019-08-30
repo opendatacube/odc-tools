dde91f41908dab637c66cea3913ec73cadb76b21
Kirill Kouzoubov <kirill888@gmail.com>
2019-08-30 16:31:56 +1000

    Update dev install script
    - do install aio
    - dscache now has dependency on index so needs to be installed later

--- scripts/dev_install.sh
+++ scripts/dev_install.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 # order is important: need to install dependencies first
-# leaf nodes: io aws ppt geom dscache
+# leaf nodes: io aws ppt geom
 # nodes with odc.* dependencies:
 # ------------------------------
-#   dtools <: aws
-#   index  <: io
-#   ui     <: index
-#   aio    <: aws ppt     # skipping aio install, has hard botocore constraints
+#   dtools  <: aws
+#   index   <: io
+#   dscache <: index
+#   ui      <: index
+#   aio     <: aws ppt
 
-LIBS="io aws ppt geom dscache"
-LIBS+=" dtools index ui"
+LIBS="io aws ppt geom"
+LIBS+=" dtools index dscache ui"
+LIBS+=" aio"
 
+# if aio enabled install aiobotocore
+if [ -z "${LIBS##*aio*}" ] ; then
+    pip install -U 'aiobotocore[boto3]'
+fi
 
 for lib in $LIBS; do
     echo "Installing odc-${lib}"
