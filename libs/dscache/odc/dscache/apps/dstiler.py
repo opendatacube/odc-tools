2dd45fbc82a949525433d9d96d9498eff80e93b3
Kirill Kouzoubov <kirill888@gmail.com>
2020-06-18 10:12:13 +1000

    Changing dstiler grid spec management
    1. Adding `--grid=<name|crs;resolution;shape>`
    2. Removing ability to customize "group name format"
       It is now always 'name:str/x:int/y:int'

--- libs/dscache/odc/dscache/apps/dstiler.py
+++ libs/dscache/odc/dscache/apps/dstiler.py
@@ -1,58 +1,59 @@
 import click
 from odc import dscache
-from odc.dscache.tools.tiling import bin_by_native_tile, web_gs, extract_native_albers_tile
-from datacube.model import GridSpec
+from odc.dscache.tools.tiling import (
+    bin_by_native_tile,
+    web_gs,
+    extract_native_albers_tile,
+    parse_gridspec,
+    mk_group_name)
 from odc.index import bin_dataset_stream
-
-GS_ALBERS = GridSpec(crs='EPSG:3577',
-                     tile_size=(100000.0, 100000.0),
-                     resolution=(-25, 25))
 
 
 @click.command('dstiler')
 @click.option('--native', is_flag=True, help='Use Landsat Path/Row as grouping')
-@click.option('--native-albers', is_flag=True, help='When datasets are in Albers grid already')
+@click.option('--native-albers', is_flag=True, help='When datasets are in Albers (AU) grid already')
 @click.option('--web', type=int, help='Use web map tiling regime at supplied zoom level')
-@click.option('--crs', type=str, help="Custom gridspec: CRS code")
-@click.option('--resolution', type=str, help="Custom gridspec: resolution in CRS units per pixel in y,x order")
-@click.option('--shape', type=str, help="Custom gridspec: shape of tile in pixel in y,x order")
-@click.option('--bin-format', type=str, help="Custom gridspec: format of bin group key")
+@click.option('--grid', type=str,
+              help="Grid spec or name 'crs;pixel_resolution;shape_in_pixels'|albers_au_25",
+              default='albers_au_25')
 @click.argument('dbfile', type=str, nargs=1)
-def cli(native, native_albers, web, crs, resolution, shape, bin_format, dbfile):
+def cli(native, native_albers, web, grid, dbfile):
     """Add spatial grouping to file db.
 
     Default grid is Australian Albers (EPSG:3577) with 100k by 100k tiles. But
     you can also group by Landsat path/row (--native), or Google's map tiling
     regime (--web zoom_level)
 
+    \b
+    Example for custom --grid:
+      - rectangular: 'epsg:6933;-10x10;2000x3000'
+                      ^crs      ^y  ^x ^ny  ^nx
+      - square     : 'epsg:3857;10;10000'
+      - named      : albers_au_25
+                     albers_africa_10  (20,30,60 are also available)
     """
     cache = dscache.open_rw(dbfile)
     label = 'Processing {} ({:,d} datasets)'.format(dbfile, cache.count)
+    group_prefix = 'grid'
+    gs = None
 
     if native:
-        group_key_fmt = 'native/{:03d}_{:03d}'
+        group_prefix = 'native'
         binner = bin_by_native_tile
     elif native_albers:
-        group_key_fmt = 'albers/{:03d}_{:03d}'
+        group_prefix = 'albers'
         binner = lambda dss: bin_by_native_tile(dss, native_tile_id=extract_native_albers_tile)
     elif web is not None:
         gs = web_gs(web)
-        group_key_fmt = 'web_' + str(web) + '/{:03d}_{:03d}'
-        binner = lambda dss: bin_dataset_stream(gs, dss)
-    elif crs is not None or resolution is not None or shape is not None or bin_format is not None:
-        settings = dict(crs=crs, resolution=resolution, shape=shape, bin_format=bin_format)
-        missing = [key for key, value in settings.items() if value is None]
-        if missing:
-            raise ValueError('Missing options in custom grid spec: {}'.format(' '.join(missing)))
-        group_key_fmt = bin_format
-        resolution = [float(comp) for comp in resolution.split(',')]
-        shape = [float(comp) for comp in shape.split(',')]
-        tile_size = [abs(res * shp) for res, shp in zip(resolution, shape)]
-        gs = GridSpec(crs=crs, tile_size=tile_size, resolution=resolution)
+        group_prefix = 'web_' + str(web)
         binner = lambda dss: bin_dataset_stream(gs, dss)
     else:
-        group_key_fmt = 'albers/{:03d}_{:03d}'
-        binner = lambda dss: bin_dataset_stream(GS_ALBERS, dss)
+        gs = parse_gridspec(grid)
+        group_prefix = f"epsg{gs.crs.epsg:d}"
+        binner = lambda dss: bin_dataset_stream(gs, dss)
+
+    if gs is not None:
+        click.echo(f'Using gridspec: {gs}')
 
     with click.progressbar(cache.get_all(), length=cache.count, label=label) as dss:
         bins = binner(dss)
@@ -61,7 +62,7 @@ def cli(native, native_albers, web, crs, resolution, shape, bin_format, dbfile):
 
     with click.progressbar(bins.values(), length=len(bins), label='Saving') as groups:
         for group in groups:
-            k = group_key_fmt.format(*group.idx)
+            k = mk_group_name(group.idx, group_prefix)
             cache.put_group(k, group.dss)
 
 
