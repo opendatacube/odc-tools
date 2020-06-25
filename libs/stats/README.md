189e29f212af7d701b50693aabdcb6abb3fc1444
Kirill Kouzoubov <kirill888@gmail.com>
2020-06-25 14:13:48 +1000

    Adding skeleton for odc.stats

---
+++ libs/stats/README.md
@@ -0,0 +1,17 @@
+odc.stats
+=========
+
+Statistical product generation framework.
+WORK IN PROGRESS
+
+Installation
+------------
+
+```
+pip install --extra-index-url="https://packages.dea.ga.gov.au" odc-stats
+```
+
+Usage
+-----
+
+TODO
---
+++ libs/stats/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='odc_stats',
+
+    use_scm_version={"root": "../..", "relative_to": __file__},
+    setup_requires=['setuptools_scm'],
+
+    author='Open Data Cube',
+    author_email='',
+    maintainer='Open Data Cube',
+    maintainer_email='',
+
+    description='Statistical Product Generation Framework',
+    long_description='',
+
+    license='Apache License 2.0',
+
+    tests_require=['pytest'],
+    install_requires=[
+        'datacube',
+        'odc_index',
+        'odc_dscache',
+        'odc_algo',
+    ],
+
+    packages=['odc.stats'],
+    zip_safe=False,
+)
