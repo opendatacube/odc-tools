032b643eec153f1bf8364eb18b1825e121bf3e9f
Kirill Kouzoubov <kirill888@gmail.com>
2020-07-01 19:52:51 +1000

    Add --aws-unsigned option to s3-find

--- apps/cloud/odc/apps/cloud/s3_find.py
+++ apps/cloud/odc/apps/cloud/s3_find.py
@@ -1,13 +1,15 @@
 import click
 import sys
-from odc.aio import s3_find_glob
+from odc.aio import s3_find_glob, S3Fetcher
 
 
 @click.command('s3-find')
 @click.option('--skip-check', is_flag=True,
               help='Assume file exists when listing exact file rather than wildcard.')
+@click.option('--aws-unsigned', is_flag=True,
+              help='Do not sign AWS S3 requests')
 @click.argument('uri', type=str, nargs=1)
-def cli(uri, skip_check):
+def cli(uri, skip_check, aws_unsigned=None):
     """ List files on S3 bucket.
 
     Example:
@@ -34,8 +36,10 @@ def cli(uri, skip_check):
     """
     flush_freq = 100
 
+    s3 = S3Fetcher(aws_unsigned=aws_unsigned)
+
     try:
-        stream = s3_find_glob(uri, skip_check)
+        stream = s3_find_glob(uri, skip_check=skip_check, s3=s3)
         for i, o in enumerate(stream):
             print(o.url, flush=(i % flush_freq == 0))
     except ValueError as ve:
@@ -44,7 +48,6 @@ def cli(uri, skip_check):
     except Exception as e:
         click.echo(str(e), err=True)
         sys.exit(1)
-    
 
 
 if __name__ == '__main__':
