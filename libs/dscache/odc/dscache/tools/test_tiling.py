f4f928569640ba661d864dd42f40a15abb82469f
Kirill Kouzoubov <kirill888@gmail.com>
2020-07-14 19:03:13 +1000

    Multi-index for group_by_nothing

---
+++ libs/dscache/odc/dscache/tools/test_tiling.py
@@ -0,0 +1,18 @@
+from uuid import UUID
+from types import SimpleNamespace
+import datetime
+from odc.dscache.tools import group_by_nothing
+
+
+def test_group_by_nothing():
+    dss = [SimpleNamespace(id=UUID(int=0x1_000_000+i),
+                           center_time=datetime.datetime(2020, 1, i)) for i in range(1, 4)]
+
+    dss.append(SimpleNamespace(id=UUID(int=0x10), center_time=datetime.datetime(2020, 1, 1)))
+    print(dss)
+    xx = group_by_nothing(dss)
+    print(xx)
+    xx = group_by_nothing(dss, datetime.timedelta(seconds=-200))
+    print(xx)
+    for u in xx.uuid.data:
+        print(u)
