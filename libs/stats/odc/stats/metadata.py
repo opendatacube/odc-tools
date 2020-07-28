7eedb2b6da2ec18309909632e787c6af870d1943
Kirill Kouzoubov <kirill888@gmail.com>
2020-07-28 10:45:57 +1000

    Refactor metadata again

--- libs/stats/odc/stats/metadata.py
+++ libs/stats/odc/stats/metadata.py
@@ -1,77 +1,51 @@
-from uuid import UUID
-from typing import Sequence, Tuple, Dict
+from typing import Dict, Any, Tuple
 from copy import deepcopy
 from datetime import datetime, timezone, timedelta
-
-import yaml
 import click
 
+from datacube.model import GridSpec
 from odc import dscache
 from odc.index import odc_uuid
-from odc.dscache._dscache import mk_group_name
-
 from .model import OutputProduct, Task
 
 
-def task_to_metadata_dict(task: Task) -> Dict:
-    geobox = task.gridspec.tile_geobox(task.tile_index)
-    region_code = "".join(tile_index_str(task.tile_index))
-    output_product = task.output_product
-    properties = deepcopy(output_product.properties)
+def render_task_metadata(task: Task, ext: str = 'tiff') -> Dict[str, Any]:
+    product = task.product
+    geobox = task.geobox
+    region_code = product.region_code(task.tile_index)
+    properties = deepcopy(product.properties)
 
-    dataset_id = odc_uuid(sources=task.input_dataset_ids,
-                          other_tags=dict(product=output_product.product_info,
-                                          product_version=output_product.product_version,
-                                          tile=task.tile_index,
-                                          start_datetime=task.start_datetime,
-                                          end_datetime=task.end_datetime),
-                          **output_product.algo_info)
-
-    properties['datetime'] = task.start_datetime.isoformat()
     properties['dtr:start_datetime'] = task.start_datetime.isoformat()
     properties['dtr:end_datetime'] = task.end_datetime.isoformat()
-    properties['odc:processing_datetime'] = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()
     properties['odc:region_code'] = region_code
 
-    location = '{}/{}/{}'.format(output_product.location,
-                                 "/".join(tile_index_str(task.tile_index)),
-                                 task.short_time)
-
-    product_short_name = output_product.product_short_name
-    measurements = {band_name: {'path': f'{product_short_name}_{region_code}_{task.short_time}_{band_name}.tif'}
-                    for band_name in output_product.measurements}
+    measurements = {band: {'path': path}
+                    for band, path in task.paths().items()}
 
     return {
         '$schema': 'https://schemas.opendatacube.org/dataset',
-        'id': str(dataset_id),
-        'product': task.output_product.product_info,
-        'crs': str(task.gridspec.crs),
+        'id': str(task.uuid),
+        'product': product.info,
+        'location': task.metadata_path('absolute', ext='yaml'),
 
+        'crs': str(geobox.crs),
         'grids': {'default': dict(shape=list(geobox.shape),
-                                  transform=list(geobox.affine))},
+                                  transform=list(geobox.transform))},
 
-        'properties': properties,
-
-        'location': location,
         'measurements': measurements,
-
-        'lineage': {'inputs': [str(ds) for ds in task.input_dataset_ids]}
+        'properties': properties,
+        'lineage': {'inputs': [str(ds) for ds in task.input_dataset_ids]},
     }
 
 
-def tile_index_str(tile_index):
-    return [f'{dim}{index:+04d}'
-            for dim, index in zip(['x', 'y'], tile_index)]
+def clear_pixel_count_product(gridspec: GridSpec) -> OutputProduct:
+    name = 'ga_s2_clear_pixel_count'
+    short_name = 'deafrica_s2_cpc'
+    version = '0.0.0'
 
-
-def clear_pixel_count_product() -> OutputProduct:
-    product_name = 'ga_s2_clear_pixel_count'
-    product_short_name = 'deafrica_s2_cpc'
-    product_version = '0.0.0'
-
-    product_info = {
-        'name': product_name,
-        'href': f'https://collections.digitalearth.africa/product/{product_name}'
+    info = {
+        'name': name,
+        'href': f'https://collections.digitalearth.africa/product/{name}'
     }
 
     algo_info = {
@@ -81,24 +55,54 @@ def clear_pixel_count_product() -> OutputProduct:
     }
 
     bucket_name = 'deafrica-stats-processing'
-    location = f's3://{bucket_name}/{product_name}/v{product_version}'
-    measurements = ['clear', 'total']
+    location = f's3://{bucket_name}/{name}/v{version}'
+    measurements = ('clear', 'total')
 
     properties = {
-        'odc:dataset_version': '3.1.0',
         'odc:file_format': 'GeoTIFF',
         'odc:producer': 'ga.gov.au',
         'odc:product_family': 'pixel_quality_statistics'
     }
 
-    return OutputProduct(product_name=product_name,
-                         product_version=product_version,
-                         product_info=product_info,
-                         product_short_name=product_short_name,
+    return OutputProduct(name=name,
+                         version=version,
+                         info=info,
+                         short_name=short_name,
                          algo_info=algo_info,
                          location=location,
                          properties=properties,
-                         measurements=measurements)
+                         measurements=measurements,
+                         gridspec=gridspec)
+
+
+# TODO: assumes annual only for now
+def load_task(cache: dscache.DatasetCache,
+              tile_index: Tuple[int, int],
+              product: OutputProduct,
+              year: int = 0,
+              grid: str = '') -> Task:
+    if grid == '':
+        grid, *_ = cache.grids
+
+    gridspec = cache.grids[grid]
+    dss = list(ds for ds in cache.stream_grid_tile(tile_index, grid))
+    input_dataset_ids = tuple(ds.id for ds in dss)
+    geobox = gridspec.tile_geobox(tile_index)
+
+    if year == 0:
+        raise NotImplementedError("TODO: compute time period from datasets")
+
+    start_datetime = datetime(year, 1, 1, tzinfo=timezone.utc)
+    end_datetime = datetime(year+1, 1, 1, tzinfo=timezone.utc) - timedelta(microseconds=1)
+    short_time = f'{year}--P1Y'
+
+    return Task(product=product,
+                tile_index=tile_index,
+                geobox=geobox,
+                start_datetime=start_datetime,
+                end_datetime=end_datetime,
+                short_time=short_time,
+                input_dataset_ids=input_dataset_ids)
 
 
 @click.command()
@@ -107,27 +111,15 @@ def main(cache_path):
     cache = dscache.open_rw(cache_path)
     grid = list(cache.grids)[0]
     gridspec = cache.grids[grid]
+    year = 2020
 
-    start_datetime = datetime(2020, 1, 1, tzinfo=timezone.utc)
-    end_datetime = datetime(2021, 1, 1, tzinfo=timezone.utc) - timedelta(microseconds=1)
-    short_time = '2020--P1Y'
-
-    output_product = clear_pixel_count_product()
+    output_product = clear_pixel_count_product(gridspec)
 
     for tile_index, _ in cache.tiles(grid):
-        input_dataset_ids = [x.id for x in cache.stream_group(mk_group_name(tile_index, grid))]
+        task = load_task(cache, tile_index, output_product, year=year)
 
-        task = Task(output_product=output_product,
-                    gridspec=gridspec,
-                    start_datetime=start_datetime,
-                    end_datetime=end_datetime,
-                    short_time=short_time,
-                    tile_index=tile_index,
-                    input_dataset_ids=input_dataset_ids)
-
-        d = task_to_metadata_dict(task)
-        region_code = "".join(tile_index_str(tile_index))
-        cache._db.append_info_dict(f"tasks/{region_code}/", d)
+        d = render_task_metadata(task)
+        cache._db.append_info_dict(f"tasks/{tile_index[0]}/{tile_index[1]}", d)
 
 
 if __name__ == '__main__':
--- libs/stats/odc/stats/model.py
+++ libs/stats/odc/stats/model.py
@@ -1,30 +1,81 @@
-from typing import Dict, Tuple, Sequence
+from typing import Dict, Tuple
 from uuid import UUID
 from datetime import datetime
-
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from datacube.model import GridSpec
+from datacube.utils.geometry import GeoBox
+from odc.index import odc_uuid
 
 
 @dataclass
 class OutputProduct:
-    product_name: str
-    product_version: str
-    product_info: Dict[str, str]
-    product_short_name: str
+    name: str
+    version: str
+    info: Dict[str, str]
+    short_name: str
     algo_info: Dict[str, str]
     location: str
     properties: Dict[str, str]
-    measurements: Sequence[str]
+    measurements: Tuple[str, ...]
+    gridspec: GridSpec
+    period: str = '1Y'
+
+    def region_code(self, tidx: Tuple[int, int], sep='', n=4) -> str:
+        return f"x{tidx[0]:+0{n}d}{sep}y{tidx[1]:+0{n}d}"
 
 
 @dataclass
 class Task:
-    output_product: OutputProduct
-    gridspec: GridSpec
+    product: OutputProduct
+    tile_index: Tuple[int, int]
+    geobox: GeoBox
     start_datetime: datetime
     end_datetime: datetime
     short_time: str
-    tile_index: Tuple[int, int]
-    input_dataset_ids: Sequence[UUID]
+    input_dataset_ids: Tuple[UUID, ...]
+    uuid: UUID = UUID(int=0)
+
+    def __post_init__(self):
+        if self.uuid.int == 0:
+            self.uuid = odc_uuid(self.product.name,
+                                 self.product.version,
+                                 sources=self.input_dataset_ids,
+                                 time=self.short_time,
+                                 tile=self.tile_index)
+
+    @property
+    def location(self) -> str:
+        """
+        Product relative location for this task
+        """
+        return self.product.region_code(self.tile_index, '/') + '/' + self.short_time
+
+    def _prefix(self, relative_to) -> str:
+        product = self.product
+        region_code = product.region_code(self.tile_index)
+        file_prefix = f'{product.short_name}_{region_code}_{self.short_time}'
+
+        if relative_to == 'dataset':
+            return file_prefix
+        elif relative_to == 'product':
+            return self.location + '/' + file_prefix
+        else:
+            return product.location + '/' + self.location + '/' + file_prefix
+
+    def paths(self, relative_to='dataset', ext='tiff') -> Dict[str, str]:
+        """
+        Compute dictionary mapping band name to paths.
+
+        :param relative_to: dataset|product|absolute
+        """
+        prefix = self._prefix(relative_to)
+        return {band: f'{prefix}_{band}.{ext}' for band in self.product.measurements}
+
+    def metadata_path(self, relative_to='dataset', ext='yaml') -> str:
+        """
+        Compute path for metadata file.
+
+        :param relative_to: dataset|product|absolute
+        """
+        return self._prefix(relative_to) + '.' + ext
