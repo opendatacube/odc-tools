7e0d48f56bcab401a07529098a04c0f625f9a02c
Kirill Kouzoubov <kirill888@gmail.com>
2020-07-30 17:21:16 +1000

    Adding type annotations to jsoncache and dscache modules

--- libs/dscache/odc/dscache/_jsoncache.py
+++ libs/dscache/odc/dscache/_jsoncache.py
@@ -1,3 +1,4 @@
+from typing import Optional, Tuple, List, Collection, Iterator, Union, Any, Dict, Iterable
 from uuid import UUID
 import json
 import lmdb
@@ -11,8 +12,15 @@ from pathlib import Path
 
 FORMAT_VERSION = b'0001'
 
+Prefix = Union[str, bytes]
+LaxUUID = Union[str, UUID]
+Document = Dict[str, Any]
+Document_ = Union[Document,
+                  Tuple[Union[str, UUID], Document]]
+MaybeTransaction = Optional[lmdb.Transaction]
 
-def key_to_bytes(k):
+
+def key_to_bytes(k: Any) -> bytes:
     if isinstance(k, str):
         return k.encode('utf8')
     if isinstance(k, UUID):
@@ -25,26 +33,29 @@ def key_to_bytes(k):
         elif k.bit_length() < 128:
             return k.to_bytes(16, 'big')
         else:
-            return str(k).decode('utf8')
+            return str(k).encode('utf8')
     if isinstance(k, tuple):
         return functools.reduce(operator.add, map(key_to_bytes, k))
 
     raise ValueError('Key must be one of str|bytes|int|UUID|tuple')
 
 
-def uuids2bytes(uu):
+def uuids2bytes(uu: Collection[UUID]) -> bytes:
     bb = bytearray(len(uu)*16)
     for i, u in enumerate(uu):
         bb[i*16:(i+1)*16] = u.bytes
     return bytes(bb)
 
 
-def bytes2uuids(bb):
+def bytes2uuids(bb: bytes) -> List[UUID]:
     n = len(bb)//16
     return [UUID(bytes=bb[i*16:(i+1)*16]) for i in range(n)]
 
 
-def prefix_visit(tr, prefix, full_key=False, db=None):
+def prefix_visit(tr: lmdb.Transaction,
+                 prefix: Prefix,
+                 full_key: bool = False,
+                 db=None) -> Iterator[Tuple[bytes, bytes]]:
     if isinstance(prefix, str):
         prefix = prefix.encode('utf8')
 
@@ -57,7 +68,9 @@ def prefix_visit(tr, prefix, full_key=False, db=None):
         yield (k if full_key else k[n:]), v
 
 
-def dict2jsonKV(oo, prefix=None, compressor=None):
+def dict2jsonKV(oo: Document,
+                prefix: Optional[str] = None,
+                compressor: Optional[zstandard.ZstdCompressor] = None) -> Iterator[Tuple[bytes, bytes]]:
     for k, doc in oo.items():
         data = json.dumps(doc, separators=(',', ':')).encode('utf8')
         if compressor is not None:
@@ -68,7 +81,8 @@ def dict2jsonKV(oo, prefix=None, compressor=None):
         yield (key_to_bytes(k), data)
 
 
-def jsonKV2dict(kv, decompressor=None):
+def jsonKV2dict(kv: Iterable[Tuple[bytes, bytes]],
+                decompressor: Optional[zstandard.ZstdDecompressor] = None) -> Document:
     def decode(kv):
         k, doc = kv
         if decompressor is not None:
@@ -78,7 +92,8 @@ def jsonKV2dict(kv, decompressor=None):
     return {k: doc for k, doc in map(decode, kv)}
 
 
-def doc2bytes(doc, purge_id=False):
+def doc2bytes(doc: Document_,
+              purge_id: bool = False) -> Tuple[bytes, bytes]:
     ''' doc is
 
     Either:
@@ -91,21 +106,28 @@ def doc2bytes(doc, purge_id=False):
     if isinstance(doc, tuple):
         k, d = doc
     else:
-        k = doc.get('id')
+        k_ = doc.get('id')
+        if k_ not in (str, UUID):
+            raise ValueError("Expect id key to be a string or a UUID")
+
+        k = k_
         d = toolz.dissoc(doc, ['id']) if purge_id else doc
 
     if not isinstance(k, UUID):
         k = UUID(k)
 
-    k = k.bytes
-    d = json.dumps(d, separators=(',', ':')).encode('utf8')
-    return (k, d)
+    raw_k = k.bytes
+    raw_doc = json.dumps(d, separators=(',', ':')).encode('utf8')
+    return (raw_k, raw_doc)
 
 
-def train_dictionary(docs, dict_sz=8*1024):
-    """ Given a finite sequence of Datasets train zstandard compression dictionary of a given size.
+def train_dictionary(docs: Iterable[Document_],
+                     dict_sz: int = 8*1024) -> Optional[bytes]:
+    """ Given a finite sequence of Documents train zstandard compression dictionary of a given size.
 
-        Accepts both `Dataset` as well as "raw" datasets.
+        Document is either a
+         - (id, {..}) tuple
+         - {id: str|UUID, ...} a dictionary with `id` key of type UUID (possibly string formatted UUID)
 
         Will return None if input sequence is empty.
     """
@@ -117,7 +139,7 @@ def train_dictionary(docs, dict_sz=8*1024):
     return zstandard.train_dictionary(dict_sz, sample).as_bytes()
 
 
-class JsonBlobCache(object):
+class JsonBlobCache:
     """
     info:
        version: 4-bytes
@@ -137,10 +159,10 @@ class JsonBlobCache(object):
         """
 
         self._dbs = state.dbs
-        self._comp = state.comp
-        self._decomp = state.decomp
+        self._comp: Optional[zstandard.ZstdCompressor] = state.comp
+        self._decomp: zstandard.ZstdCompressor = state.decomp
         self._closed = False
-        self._current_transaction = None
+        self._current_transaction: MaybeTransaction = None
 
     def close(self):
         """Write any outstanding product/metadata definitions (if in write mode) and
@@ -156,36 +178,39 @@ class JsonBlobCache(object):
     def __del__(self):
         self.close()
 
-    def _append_info_dict(self, prefix, oo, tr):
+    def _append_info_dict(self, prefix: str, oo: Document, tr: lmdb.Transaction):
         for k, v in dict2jsonKV(oo, prefix, self._comp):
             tr.put(k, v,
                    overwrite=True,
                    dupdata=False,
                    db=self._dbs.info)
 
-    def _clear_info_dict(self, prefix, tr):
+    def _clear_info_dict(self, prefix: Prefix, tr: lmdb.Transaction):
         db_info = self._dbs.info
         for k, _ in prefix_visit(tr, prefix, full_key=True, db=db_info):
             tr.delete(k, db=db_info)
 
-    def _get_info_dict(self, prefix, tr):
+    def _get_info_dict(self, prefix: Prefix, tr: lmdb.Transaction):
         return jsonKV2dict(prefix_visit(tr, prefix, db=self._dbs.info), self._decomp)
 
-    def append_info_dict(self, prefix, oo, transaction=None):
+    def append_info_dict(self,
+                         prefix: str,
+                         oo: Document,
+                         transaction: MaybeTransaction = None):
         if transaction is None:
             with self._dbs.main.begin(write=True) as tr:
                 self._append_info_dict(prefix, oo, tr)
         else:
             self._append_info_dict(prefix, oo, transaction)
 
-    def get_info_dict(self, prefix, transaction=None):
+    def get_info_dict(self, prefix: Prefix, transaction: MaybeTransaction = None):
         if transaction is None:
             with self._dbs.main.begin(write=False) as tr:
                 return self._get_info_dict(prefix, tr)
         else:
             return self._get_info_dict(prefix, transaction)
 
-    def clear_info_dict(self, prefix, transaction=None):
+    def clear_info_dict(self, prefix: Prefix, transaction: MaybeTransaction = None):
         if transaction is None:
             with self._dbs.main.begin(write=True) as tr:
                 return self._clear_info_dict(prefix, tr)
@@ -193,23 +218,24 @@ class JsonBlobCache(object):
             return self._clear_info_dict(prefix, transaction)
 
     @property
-    def current_transaction(self):
+    def current_transaction(self) -> MaybeTransaction:
         return self._current_transaction
 
     @property
-    def readonly(self):
+    def readonly(self) -> bool:
         return self._comp is None
 
-    def _doc2kv(self, doc):
+    def _doc2kv(self, doc: Document_) -> Tuple[bytes, bytes]:
         k, d = doc2bytes(doc)
-        d = self._comp.compress(d)
+        if self._comp:
+            d = self._comp.compress(d)
         return (k, d)
 
-    def _doc_save(self, doc, transaction):
+    def _doc_save(self, doc: Document_, transaction: lmdb.Transaction):
         k, v = self._doc2kv(doc)
         transaction.put(k, v)
 
-    def bulk_save(self, docs):
+    def bulk_save(self, docs: Iterable[Document_]):
         try:
             with self._dbs.main.begin(self._dbs.ds, write=True) as tr:
                 self._current_transaction = tr
@@ -218,7 +244,10 @@ class JsonBlobCache(object):
         finally:
             self._current_transaction = None
 
-    def tee(self, docs, max_transaction_size=10000, transform=None):
+    def tee(self,
+            docs: Iterable[Any],
+            max_transaction_size: int = 10000,
+            transform=None) -> Iterator[Any]:
         """Given a lazy stream of (k,v) pairs persist them to disk and then pass through
         for further processing.
         :docs: stream of documents (uuid, {..}) pairs
@@ -241,7 +270,7 @@ class JsonBlobCache(object):
         finally:
             self._current_transaction = None
 
-    def put_group(self, name, uuids):
+    def put_group(self, name: str, uuids: Collection[UUID]):
         """ Group is a named list of uuids
         """
         data = uuids2bytes(uuids)
@@ -250,19 +279,22 @@ class JsonBlobCache(object):
         with self._dbs.main.begin(self._dbs.groups, write=True) as tr:
             tr.put(k, data)
 
-    def _get_group_raw(self, name):
+    def _get_group_raw(self, name: Prefix) -> bytes:
         k = key_to_bytes(name)
 
         with self._dbs.main.begin(self._dbs.groups, write=False) as tr:
             return tr.get(k)
 
-    def get_group(self, name):
+    def get_group(self, name: str) -> Optional[List[UUID]]:
         """ Group is a named list of uuids
         """
         data = self._get_group_raw(key_to_bytes(name))
         return bytes2uuids(data) if data is not None else None
 
-    def groups(self, raw=False, prefix=None):
+    def groups(self,
+               raw: bool = False,
+               prefix: Optional[Prefix] = None) -> Union[List[Tuple[bytes, int]],
+                                                         List[Tuple[str, int]]]:
         """Get list of tuples (group_name, group_size).
 
         :raw bool: Normally names are returned as strings, supplying raw=True
@@ -285,11 +317,11 @@ class JsonBlobCache(object):
         nn = _raw(prefix)
         return nn if raw else [(n.decode('utf8'), c) for n, c in nn]
 
-    def _extract_ds(self, d):
+    def _extract_ds(self, d: bytes) -> Document:
         d = self._decomp.decompress(d)
         return json.loads(d)
 
-    def get(self, uuid):
+    def get(self, uuid: LaxUUID) -> Optional[Document]:
         """Extract single dataset with a given uuid, or return None if not found"""
         if isinstance(uuid, str):
             uuid = UUID(uuid)
@@ -303,7 +335,7 @@ class JsonBlobCache(object):
 
             return self._extract_ds(d)
 
-    def get_all(self):
+    def get_all(self) -> Iterator[Tuple[UUID, Document]]:
         try:
             with self._dbs.main.begin(self._dbs.ds, buffers=True) as tr:
                 self._current_transaction = tr
@@ -312,7 +344,7 @@ class JsonBlobCache(object):
         finally:
             self._current_transaction = None
 
-    def stream_group(self, group_name):
+    def stream_group(self, group_name: str) -> Iterator[Tuple[UUID, Document]]:
         uu = self._get_group_raw(group_name)
         if uu is None:
             raise ValueError('No such group: %s' % group_name)
@@ -330,12 +362,23 @@ class JsonBlobCache(object):
                 yield UUID(bytes=bytes(key)), self._extract_ds(d)
 
     @property
-    def count(self):
+    def count(self) -> int:
         with self._dbs.main.begin(self._dbs.ds) as tr:
             return tr.stat()['entries']
 
 
-def maybe_delete_db(path):
+def db_exists(path: str) -> bool:
+    path = Path(path)
+    if not path.exists():
+        return False
+
+    if path.is_dir():
+        return Path(path/"data.mdb").exists()
+
+    return True
+
+
+def maybe_delete_db(path: str) -> bool:
     """ Delete existing database if it exists.
 
         LMDB database consists of two files, data + lock, they can be arranged in two possible layouts:
@@ -365,7 +408,7 @@ def maybe_delete_db(path):
     return True
 
 
-def _from_existing_db(db, complevel=6):
+def _from_existing_db(db, complevel: int = 6) -> JsonBlobCache:
     readonly = db.flags().get('readonly')
 
     try:
@@ -401,8 +444,8 @@ def _from_existing_db(db, complevel=6):
 
 
 def _from_empty_db(db,
-                   complevel=6,
-                   zdict=None):
+                   complevel: int = 6,
+                   zdict: Optional[bytes] = None):
     assert isinstance(zdict, (bytes, type(None)))
 
     db_info = db.open_db(b'info', create=True)
@@ -431,22 +474,18 @@ def _from_empty_db(db,
     return JsonBlobCache(state)
 
 
-def open_ro(path,
-            lock=False):
+def open_ro(path: str,
+            lock: bool = False) -> JsonBlobCache:
     """Open existing database in readonly mode.
 
-    NOTE: default mode assumes db file is static (not being modified
-    externally), if this is not the case, supply `lock=True` parameter.
+    .. note:
 
-    :path str: Path to the db could be folder or actual file
+       default mode assumes db file is static (not being modified
+       externally), if this is not the case, supply ``lock=True`` parameter.
 
-    :products: Override product dictionary with compatible products loaded from
-    the datacube database, this is generally only needed if you intend to add
-    datasets to the datacube index directly (i.e. without product matching
-    metadata documents).
+    :path: Path to the db could be folder or actual file
 
-    :lock bool: Supply True if external process is changing DB concurrently.
-
+    :lock: Supply ``True`` if external process is changing DB concurrently.
     """
 
     subdir = Path(path).is_dir()
@@ -461,9 +500,9 @@ def open_ro(path,
     return _from_existing_db(db)
 
 
-def open_rw(path,
-            max_db_sz=None,
-            complevel=6):
+def open_rw(path: str,
+            max_db_sz: Optional[int] = None,
+            complevel: int = 6) -> JsonBlobCache:
     """Open existing database in append mode.
 
     :path str: Path to the db could be folder or actual file
@@ -495,11 +534,11 @@ def open_rw(path,
     return _from_existing_db(db, complevel=complevel)
 
 
-def create_cache(path,
-                 complevel=6,
+def create_cache(path: str,
+                 complevel: int = 6,
                  zdict=None,
-                 max_db_sz=None,
-                 truncate=False):
+                 max_db_sz: Optional[int] = None,
+                 truncate: bool = False):
     """Create new file database or open existing one.
 
     :path str: Path where to create new database (this will be a directory with 2 files in it)
