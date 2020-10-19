09eb1ca6325c9ec781d148ed2d86b2210599d8f7
Toktam <toktam.ebadi@ga.gov.au>
2020-10-19 11:30:36 +1100

    Removed odc_aio dependency from dc_tools.

--- apps/dc_tools/setup.py
+++ apps/dc_tools/setup.py
@@ -25,8 +25,12 @@ setup(
         'datacube',
         'odc_index',
         'odc_io',
+<<<<<<< HEAD
         'odc_aws',
         'odc_aio'
+=======
+        'odc_aws'
+>>>>>>> Removed odc_aio dependency from dc_tools.
     ],
 
     extras_require={
