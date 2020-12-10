bf96d11df4fa7cbaf35a25ac5a386ec1e2464d97
Toktam <toktam.ebadi@ga.gov.au>
2020-12-10 11:34:13 +1100

    Removed odc-ows from the list of dependencies.

--- libs/stats/setup.py
+++ libs/stats/setup.py
@@ -1,37 +1,26 @@
 from setuptools import setup
 
 setup(
-    name='odc_stats',
-
+    name="odc_stats",
     use_scm_version={"root": "../..", "relative_to": __file__},
-    setup_requires=['setuptools_scm'],
-
-    author='Open Data Cube',
-    author_email='',
-    maintainer='Open Data Cube',
-    maintainer_email='',
-
-    description='Statistical Product Generation Framework',
-    long_description='',
-
-    license='Apache License 2.0',
-
-    tests_require=['pytest'],
+    setup_requires=["setuptools_scm"],
+    author="Open Data Cube",
+    author_email="",
+    maintainer="Open Data Cube",
+    maintainer_email="",
+    description="Statistical Product Generation Framework",
+    long_description="",
+    license="Apache License 2.0",
+    tests_require=["pytest"],
     install_requires=[
-        'datacube',
-        'odc_index',
-        'odc_dscache',
-        'odc_algo',
-        'dataclasses',
-        'odc_ows'
+        "datacube",
+        "odc_index",
+        "odc_dscache",
+        "odc_algo",
+        "dataclasses",
+        "tqdm",
     ],
-
-    packages=['odc.stats'],
+    packages=["odc.stats"],
     zip_safe=False,
-
-    entry_points={
-        'console_scripts': [
-            'odc-stats = odc.stats.cli:main'
-        ]
-    }
+    entry_points={"console_scripts": ["odc-stats = odc.stats.cli:main"]},
 )
