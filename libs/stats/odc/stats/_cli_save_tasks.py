e606225c4b9aa4a008af3207f5a0e31bb47c5572
Kirill Kouzoubov <kirill888@gmail.com>
2021-02-11 17:33:31 +1100

    Fix typo in help message 39->30

--- libs/stats/odc/stats/_cli_save_tasks.py
+++ libs/stats/odc/stats/_cli_save_tasks.py
@@ -9,7 +9,7 @@ from ._cli_common import main
     "--grid",
     type=str,
     help=(
-        "Grid name or spec: au-{10|20|39|60},africa-{10|20|30|60}, albers-au-25 (legacy one)"
+        "Grid name or spec: au-{10|20|30|60},africa-{10|20|30|60}, albers-au-25 (legacy one)"
         "'crs;pixel_resolution;shape_in_pixels'"
     ),
     prompt="""Enter GridSpec
