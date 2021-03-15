d696a6a58551a29fad58cc335dbe63ab920ced04
Kirill Kouzoubov <kirill888@gmail.com>
2021-03-15 15:45:37 +1100

    Change default version expansion to match DEA path rules
    Version in paths ought to have `-` instead of `.`, if you want `.` use
    `{version_raw}` when configuring output template.

--- libs/stats/tests/test_proc.py
+++ libs/stats/tests/test_proc.py
@@ -6,7 +6,7 @@ def test_runner_product_cfg(test_db_path, dummy_plugin_name):
         filedb=test_db_path,
         plugin=dummy_plugin_name,
         product={"name": "ga_test", "short_name": "tt", "version": "3.2.1"},
-        output_location="s3://data-bucket/{product}/v{version}",
+        output_location="s3://data-bucket/{product}/{version}",
         plugin_config={"bands": ["red", "nir"]},
         cog_opts={
             "compression": "deflate",
@@ -19,7 +19,7 @@ def test_runner_product_cfg(test_db_path, dummy_plugin_name):
     assert runner.product.name == "ga_test"
     assert runner.product.short_name == "tt"
     assert runner.product.version == "3.2.1"
-    assert runner.product.location == "s3://data-bucket/ga_test/v3.2.1"
+    assert runner.product.location == "s3://data-bucket/ga_test/3-2-1"
     assert runner.product.measurements == ("red", "nir")
 
     cfg = runner.sink.cog_opts("red")
