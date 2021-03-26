38409bfaf4d026bcbfe20be92baaa5d5a1ded455
Kirill Kouzoubov <kirill888@gmail.com>
2021-03-26 13:10:28 +1100

    Change Wofs counting method
    - use int16 dtype for counts
    - skip pixels that have any badness in them not just cloud/shadow
    - keep track of pixels with some data and set counts to -999 when
      there were no pixels at all
    - set version

--- libs/stats/odc/stats/_wofs.py
+++ libs/stats/odc/stats/_wofs.py
@@ -5,7 +5,7 @@ from typing import Optional, Tuple
 import xarray as xr
 from odc.stats.model import Task
 from odc.algo.io import load_with_native_transform
-from odc.algo import safe_div, apply_numexpr
+from odc.algo import safe_div, apply_numexpr, keep_good_only
 from .model import StatsPluginInterface
 from . import _plugins
 
@@ -13,7 +13,7 @@ from . import _plugins
 class StatsWofs(StatsPluginInterface):
     NAME = "ga_ls_wo_summary"
     SHORT_NAME = NAME
-    VERSION = '0.0.0'
+    VERSION = "1.6.0"
     PRODUCT_FAMILY = "wo_summary"
 
     def __init__(
@@ -26,29 +26,60 @@ class StatsWofs(StatsPluginInterface):
         return ("count_wet", "count_clear", "frequency")
 
     def _native_tr(self, xx):
+        """
+        xx.water -- uint8 classifier bitmask
+
+        .. code-block::
+
+          |128| 64| 32| 16| |  8|  4|  2|  1|
+          |---|---|---|---|=|---|---|---|---|
+            7   6   5   4     3   2   1   0
+            |   |   |   |     |   |   |   |
+            |   |   |   |     |   |   |   x---> NODATA: 1 -- all bands were nodata
+            |   |   |   |     |   |   o-------> Non Contigous - some bands were nodata)
+            |   |   |   |     |   x-----------> Low Solar Angle
+            |   |   |   |     o---------------> Terrain Shadow
+            |   |   |   |
+            |   |   |   x---------------------> Terrain High Slope
+            |   |   o-------------------------> Cloud Shadow
+            |   x-----------------------------> Cloud
+            o---------------------------------> Water
+
+        """
         wet = xx.water == 128
         dry = xx.water == 0
-        # Cloud is
-        #  - valid observation bits 0,1 == 0
-        #  - is cloud shadow or cloud (one of bits 5,6 is non-zero)
-        cloud = apply_numexpr("((water%4)==0) & ((water>>5)%4 > 0)", xx, dtype="bool")
-        cloud.attrs.pop("nodata", None)
-        return xr.Dataset(dict(wet=wet, dry=dry, cloud=cloud))
+        # bad -- if any bit in [1, 6] range is non zero
+        #        X--- ---X
+        #        7654 3210
+        # Erase bits 7 and 0 and check if what's left is non-zero
+        bad = apply_numexpr("((water%128)>>1) > 0", xx, dtype="bool")
+        bad.attrs.pop("nodata", None)
+
+        # some -- nodata flag is not set and contiguity flag is not set
+        some = apply_numexpr("((water)%4) == 0", xx, dtype="bool")
+        some.attrs.pop("nodata", None)
+        return xr.Dataset(dict(wet=wet, dry=dry, bad=bad, some=some))
 
     def _fuser(self, xx):
+        """
+        xx.bad  -- don't count
+        xx.wet  -- is wet
+        xx.dry  -- is dry
+        xx.some -- there was at least one non-nodata observation at that pixel
+        """
         from odc.algo._masking import _or_fuser
 
         # Merge everything with OR first
         xx = xx.map(_or_fuser)
 
         # Ensure all 3 bits are exclusive
-        #  cloud=T, wet=?, dry=? => (wet'=F  , dry'=F)
-        #  cloud=F, wet=T, dry=T => (wet'=F  , dry'=F)
-        #  else                  => (wet'=wet, dry'=dry)
-        wet = apply_numexpr("wet & (~dry) & (~cloud)", xx, dtype="bool")
-        dry = apply_numexpr("dry & (~wet) & (~cloud)", xx, dtype="bool")
+        #  bad=T, wet=?, dry=? => (wet'=F  , dry'=F)
+        #  bad=F, wet=T, dry=T => (wet'=F  , dry'=F)
+        #  else                => (wet'=wet, dry'=dry)
+        wet = apply_numexpr("wet & (~dry) & (~bad)", xx, dtype="bool")
+        dry = apply_numexpr("dry & (~wet) & (~bad)", xx, dtype="bool")
 
-        return xr.Dataset(dict(wet=wet, dry=dry, cloud=xx.cloud))
+        return xr.Dataset(dict(wet=wet, dry=dry, bad=xx.bad, some=xx.some))
 
     def input_data(self, task: Task) -> xr.Dataset:
         chunks = {"y": -1, "x": -1}
@@ -68,13 +99,22 @@ class StatsWofs(StatsPluginInterface):
         return xx
 
     def reduce(self, xx: xr.Dataset) -> xr.Dataset:
-        count_wet = xx.wet.sum(axis=0, dtype="uint16")
-        count_dry = xx.dry.sum(axis=0, dtype="uint16")
+        nodata = -999
+        count_some = xx.some.sum(axis=0, dtype="int16")
+        count_wet = xx.wet.sum(axis=0, dtype="int16")
+        count_dry = xx.dry.sum(axis=0, dtype="int16")
         count_clear = count_wet + count_dry
         frequency = safe_div(count_wet, count_clear, dtype="float32")
 
+        count_wet.attrs["nodata"] = nodata
+        count_clear.attrs["nodata"] = nodata
+
+        is_ok = count_some > 0
+        count_wet = keep_good_only(count_wet, is_ok)
+        count_clear = keep_good_only(count_clear, is_ok)
+
         return xr.Dataset(
-            dict(count_wet=count_wet, count_clear=count_clear, frequency=frequency)
+            dict(count_wet=count_wet, count_clear=count_clear, frequency=frequency,)
         )
 
     def rgba(self, xx: xr.Dataset) -> Optional[xr.DataArray]:
