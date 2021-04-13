316674158510ea4696928a3072b74fc4c468083a
Kirill Kouzoubov <kirill888@gmail.com>
2021-04-13 15:34:34 +1000

    Splitting GM Plugin into S2/LS/Generic
    Generic doesn't have band/mask/cloud defaults and S2/LS do.

--- libs/stats/odc/stats/_gm.py
+++ libs/stats/odc/stats/_gm.py
@@ -11,60 +11,45 @@ from .model import StatsPluginInterface
 from . import _plugins
 
 
-class StatsGMS2(StatsPluginInterface):
-    NAME = "gm_s2_annual"
+class StatsGM(StatsPluginInterface):
+    NAME = "gm"
     SHORT_NAME = NAME
     VERSION = "0.0.0"
     PRODUCT_FAMILY = "geomedian"
 
     def __init__(
         self,
-        resampling: str = "bilinear",
-        bands: Optional[Tuple[str, ...]] = None,
-        filters: Optional[Tuple[int, int]] = (2, 5),
-        work_chunks: Tuple[int, int] = (400, 400),
-        mask_band: str = "SCL",
-        cloud_classes=(
-            "cloud shadows",
-            "cloud medium probability",
-            "cloud high probability",
-            "thin cirrus",
-        ),
+        bands: Tuple[str, ...],
+        mask_band: str,
+        cloud_classes: Tuple[str, ...],
+        nodata_classes: Optional[Tuple[str, ...]] = None,
+        filters: Optional[Tuple[int, int]] = (0, 0),
         basis_band=None,
-        nodata_class: Optional[str] = None,
-        aux_names=dict(smad="SMAD", emad="EMAD", bcmad="BCMAD", count="COUNT"),
+        aux_names=dict(smad="smad", emad="emad", bcmad="bcmad", count="count"),
         rgb_bands=None,
         rgb_clamp=(1, 3_000),
+        resampling: str = "bilinear",
+        work_chunks: Tuple[int, int] = (400, 400),
     ):
-        if bands is None:
-            bands = (
-                "B02",
-                "B03",
-                "B04",
-                "B05",
-                "B06",
-                "B07",
-                "B08",
-                "B8A",
-                "B11",
-                "B12",
-            )
-            if rgb_bands is None:
-                rgb_bands = ("B04", "B03", "B02")
-
-        self.resampling = resampling
         self.bands = tuple(bands)
         self._basis_band = basis_band or self.bands[0]
+
+        if nodata_classes is not None:
+            nodata_classes = tuple(nodata_classes)
+
+        self._mask_band = mask_band
+        self.cloud_classes = tuple(cloud_classes)
+        self._nodata_classes = nodata_classes
+        self.filters = filters
+
         self._renames = aux_names
-        self.rgb_bands = rgb_bands
-        self.rgb_clamp = rgb_clamp
         self.aux_bands = tuple(
             self._renames.get(k, k) for k in ("smad", "emad", "bcmad", "count")
         )
-        self._mask_band = mask_band
-        self._nodata_class = nodata_class
-        self.filters = filters
-        self.cloud_classes = tuple(cloud_classes)
+        self.rgb_bands = rgb_bands
+        self.rgb_clamp = rgb_clamp
+
+        self.resampling = resampling
         self._work_chunks = work_chunks
 
     @property
@@ -82,7 +67,7 @@ class StatsGMS2(StatsPluginInterface):
         #  xx[mask == nodata] = nodata
         mask = xx[self._mask_band]
         xx = xx.drop_vars([self._mask_band])
-        keeps = enum_to_bool(mask, [self._nodata_class], invert=True)
+        keeps = enum_to_bool(mask, self._nodata_classes, invert=True)
         xx = keep_good_only(xx, keeps)
 
         return xx
@@ -104,7 +89,9 @@ class StatsGMS2(StatsPluginInterface):
         )
 
         bands_to_load = self.bands
-        if self._nodata_class is not None:
+        if self._nodata_classes is not None:
+            # NOTE: this ends up loading Mask band twice, once to compute
+            # ``.erase`` band and once to compute ``nodata`` mask.
             bands_to_load = (*bands_to_load, self._mask_band)
 
         xx = load_with_native_transform(
@@ -146,4 +133,117 @@ class StatsGMS2(StatsPluginInterface):
         return to_rgba(xx, clamp=self.rgb_clamp, bands=self.rgb_bands)
 
 
+_plugins.register("gm-generic", StatsGM)
+
+
+class StatsGMS2(StatsGM):
+    NAME = "gm_s2_annual"
+    SHORT_NAME = NAME
+    VERSION = "0.0.0"
+    PRODUCT_FAMILY = "geomedian"
+
+    def __init__(
+        self,
+        bands: Optional[Tuple[str, ...]] = None,
+        mask_band: str = "SCL",
+        cloud_classes: Tuple[str, ...] = (
+            "cloud shadows",
+            "cloud medium probability",
+            "cloud high probability",
+            "thin cirrus",
+        ),
+        nodata_classes: Optional[Tuple[str, ...]] = None,
+        filters: Optional[Tuple[int, int]] = (2, 5),
+        basis_band: Optional[str] = None,
+        aux_names=dict(smad="SMAD", emad="EMAD", bcmad="BCMAD", count="COUNT"),
+        rgb_bands=None,
+        rgb_clamp=(1, 3_000),
+        resampling: str = "bilinear",
+        work_chunks: Tuple[int, int] = (400, 400),
+        **other,
+    ):
+        if bands is None:
+            bands = (
+                "B02",
+                "B03",
+                "B04",
+                "B05",
+                "B06",
+                "B07",
+                "B08",
+                "B8A",
+                "B11",
+                "B12",
+            )
+            if rgb_bands is None:
+                rgb_bands = ("B04", "B03", "B02")
+
+        super().__init__(
+            bands=bands,
+            mask_band=mask_band,
+            cloud_classes=cloud_classes,
+            nodata_classes=nodata_classes,
+            filters=filters,
+            basis_band=basis_band,
+            aux_names=aux_names,
+            rgb_bands=rgb_bands,
+            rgb_clamp=rgb_clamp,
+            resampling=resampling,
+            work_chunks=work_chunks,
+            **other,
+        )
+
+
 _plugins.register("gm-s2", StatsGMS2)
+
+
+class StatsGMLS(StatsGM):
+    NAME = "gm_ls_annual"
+    SHORT_NAME = NAME
+    VERSION = "3.0.0"
+    PRODUCT_FAMILY = "geomedian"
+
+    def __init__(
+        self,
+        bands: Optional[Tuple[str, ...]] = None,
+        mask_band: str = "fmask",
+        cloud_classes: Tuple[str, ...] = ("cloud", "shadow"),
+        nodata_classes: Optional[Tuple[str, ...]] = ("nodata",),
+        filters: Optional[Tuple[int, int]] = (0, 0),
+        basis_band: Optional[str] = None,
+        aux_names=dict(smad="sdev", emad="edev", bcmad="bcdev", count="count"),
+        rgb_bands=None,
+        rgb_clamp=(1, 3_000),
+        resampling: str = "bilinear",
+        work_chunks: Tuple[int, int] = (400, 400),
+        **other,
+    ):
+        if bands is None:
+            bands = (
+                "red",
+                "green",
+                "blue",
+                "nir",
+                "swir1",
+                "swir2",
+            )
+            if rgb_bands is None:
+                rgb_bands = ("red", "green", "blue")
+
+        super().__init__(
+            bands=bands,
+            mask_band=mask_band,
+            cloud_classes=cloud_classes,
+            nodata_classes=nodata_classes,
+            filters=filters,
+            basis_band=basis_band,
+            aux_names=aux_names,
+            rgb_bands=rgb_bands,
+            rgb_clamp=rgb_clamp,
+            resampling=resampling,
+            work_chunks=work_chunks,
+            **other,
+        )
+
+
+_plugins.register("gm-ls", StatsGMLS)
