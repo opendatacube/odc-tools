24169d01e6c6d8d04edcb56b278e48f8bd7c4835
Kirill Kouzoubov <kirill888@gmail.com>
2021-05-25 18:17:29 +1000

    Add dc_load non-db version of dc.load

--- libs/algo/odc/algo/io.py
+++ libs/algo/odc/algo/io.py
@@ -1,4 +1,16 @@
-from typing import Optional, List, Iterable, Union, Dict, Tuple, Callable, cast
+from typing import (
+    Any,
+    Optional,
+    List,
+    Iterable,
+    Union,
+    Dict,
+    Tuple,
+    Callable,
+    cast,
+    Sequence,
+)
+from warnings import warn
 import xarray as xr
 
 from odc.index import group_by_nothing, solar_offset
@@ -6,6 +18,7 @@ from odc.algo import enum_to_bool, xr_reproject
 from datacube import Datacube
 from datacube.model import Dataset
 from datacube.utils.geometry import GeoBox, gbox
+from datacube.api.core import output_geobox
 from datacube.testutils.io import native_geobox
 from ._masking import _max_fuser, _nodata_fuser
 
@@ -46,6 +59,77 @@ def _split_by_grid(xx: xr.DataArray) -> List[xr.DataArray]:
         return yy
 
     return [extract(ii) for ii in xx.groupby(xx.grid).groups.values()]
+
+
+def dc_load(
+    datasets: Sequence[Dataset],
+    measurements: Optional[Union[str, Sequence[str]]] = None,
+    geobox: Optional[GeoBox] = None,
+    groupby: Optional[str] = None,
+    resampling: Optional[Union[str, Dict[str, str]]] = None,
+    skip_broken_datasets: bool = False,
+    chunks: Optional[Dict[str, int]] = None,
+    progress_cbk: Optional[Callable[[int, int], Any]] = None,
+    fuse_func=None,
+    **kw,
+) -> xr.Dataset:
+    assert len(datasets) > 0
+
+    # dask_chunks is a backward-compatibility alias for chunks
+    if chunks is None:
+        chunks = kw.pop('dask_chunks', None)
+    # group_by is a backward-compatibility alias for groupby
+    if groupby is None:
+        groupby = kw.pop("group_by", "time")
+    # bands alias for measurements
+    if measurements is None:
+        measurements = kw.pop("bands", None)
+
+    # extract all "output_geobox" inputs
+    geo_keys = {
+        k: kw.pop(k)
+        for k in [
+            "like",
+            "geopolygon",
+            "resolution",
+            "output_crs",
+            "crs",
+            "align",
+            "x",
+            "y",
+            "lat",
+            "lon",
+        ]
+        if k in kw
+    }
+
+    ds = datasets[0]
+    product = ds.type
+
+    if geobox is None:
+        if len(geo_keys) == 0:
+            # TODO: figure out extent to load based on supplied datasets
+            raise NotImplementedError("Have to supply region to load (for now)")
+
+        geobox = output_geobox(
+            grid_spec=product.grid_spec, load_hints=product.load_hints(), **geo_keys
+        )
+    elif len(geo_keys):
+        warn(f"Supplied 'geobox=' parameter aliases {list(geo_keys)} inputs")
+
+    grouped = Datacube.group_datasets(datasets, groupby)
+    mm = product.lookup_measurements(measurements)
+    return Datacube.load_data(
+        grouped,
+        geobox,
+        mm,
+        resampling=resampling,
+        fuse_func=fuse_func,
+        dask_chunks=chunks,
+        skip_broken_datasets=skip_broken_datasets,
+        progress_cbk=progress_cbk,
+        **kw,
+    )
 
 
 def _load_with_native_transform_1(
@@ -100,7 +184,7 @@ def load_with_native_transform(
     chunks: Optional[Dict[str, int]] = None,
     load_chunks: Optional[Dict[str, int]] = None,
     pad: Optional[int] = None,
-    **kw
+    **kw,
 ) -> xr.Dataset:
     """
     Load a bunch of datasets with native pixel transform.
@@ -182,7 +266,7 @@ def load_enum_mask(
     resampling: str = "nearest",
     groupby: Optional[str] = None,
     chunks: Optional[Dict[str, int]] = None,
-    **kw
+    **kw,
 ) -> xr.DataArray:
     """
     Load enumerated mask (like fmask).
@@ -214,7 +298,7 @@ def load_enum_mask(
         groupby=groupby,
         chunks=chunks,
         fuser=_max_fuser,
-        **kw
+        **kw,
     )
     return xx[band] > 127
 
@@ -228,7 +312,7 @@ def load_enum_filtered(
     groupby: Optional[str] = None,
     resampling: str = "nearest",
     chunks: Optional[Dict[str, int]] = None,
-    **kw
+    **kw,
 ) -> xr.DataArray:
     """
     Load enumerated mask (like fmask/SCL) with native pixel filtering.
@@ -270,7 +354,8 @@ def load_enum_filtered(
     def native_op(xx: xr.Dataset) -> xr.Dataset:
         _xx = enum_to_bool(xx[band], categories)
         return xr.Dataset(
-            {band: _xx}, attrs={"native": True},  # <- native flag needed for fuser
+            {band: _xx},
+            attrs={"native": True},  # <- native flag needed for fuser
         )
 
     def fuser(xx: xr.Dataset) -> xr.Dataset:
@@ -306,7 +391,7 @@ def load_enum_filtered(
         resampling=resampling,
         chunks=chunks,
         pad=pad,
-        **kw
+        **kw,
     )[band]
     assert isinstance(xx, xr.DataArray)
     return xx
