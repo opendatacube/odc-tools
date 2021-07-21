0ab4dd98c2655bcf096a809fbe02f4cc41573310
Rodrigo <rodrigo.carvalho@ga.gov.au>
2021-07-21 15:46:18 +1000

    comments options and test

--- libs/aws/odc/aws/inventory.py
+++ libs/aws/odc/aws/inventory.py
@@ -21,7 +21,7 @@ def find_latest_manifest(prefix, s3, **kw) -> str:
                 return d + "manifest.json"
 
 
-def retrieve_manifest_files(key: str, s3, schema, **kw) -> SimpleNamespace:
+def retrieve_manifest_files(key: str, s3, schema, **kw):
     """
     Retrieve manifest file and return a namespace
 
@@ -40,7 +40,15 @@ def retrieve_manifest_files(key: str, s3, schema, **kw) -> SimpleNamespace:
         yield rec
 
 
-def list_inventory(manifest, s3=None, prefix: str = '', suffix: str = '', contains: str = '', **kw) -> SimpleNamespace:
+def list_inventory(
+        manifest,
+        s3=None,
+        prefix: str = '',
+        suffix: str = '',
+        contains: str = '',
+        n_threads: int = None,
+        **kw
+):
     """
     Returns a generator of inventory records
 
@@ -51,6 +59,7 @@ def list_inventory(manifest, s3=None, prefix: str = '', suffix: str = '', contai
     :param prefix: (str)
     :param suffix: (str)
     :param contains: (str)
+    :param n_threads: (int) number of threads, if not sent does not use threads
     :return: SimpleNamespace
     """
     s3 = s3 or s3_client()
@@ -73,19 +82,34 @@ def list_inventory(manifest, s3=None, prefix: str = '', suffix: str = '', contai
     data_urls = [s3_prefix + f["key"] for f in info["files"]]
     schema = tuple(info["fileSchema"].split(", "))
 
-    with ThreadPoolExecutor(max_workers=1000) as executor:
-        tasks = [
-            executor.submit(
-                retrieve_manifest_files,
-                key,
-                s3,
-                schema
-            )
-            for key in data_urls
-        ]
+    if n_threads:
+        with ThreadPoolExecutor(max_workers=1000) as executor:
+            tasks = [
+                executor.submit(
+                    retrieve_manifest_files,
+                    key,
+                    s3,
+                    schema
+                )
+                for key in data_urls
+            ]
 
-        for future in as_completed(tasks):
-            for namespace in future.result():
+            for future in as_completed(tasks):
+                for namespace in future.result():
+                    key = namespace.Key
+                    if (
+                            key.startswith(prefix) and
+                            key.endswith(suffix) and
+                            contains in key
+                    ):
+                        yield namespace
+    else:
+        for u in data_urls:
+            for namespace in retrieve_manifest_files(
+                    u,
+                    s3,
+                    schema
+            ):
                 key = namespace.Key
                 if (
                         key.startswith(prefix) and
