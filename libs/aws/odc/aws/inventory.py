aa5b030f8c067596b1e63d54da63dcb824031370
Rodrigo <rodrigo.carvalho@ga.gov.au>
2021-07-21 14:38:08 +1000

    comments and format

--- libs/aws/odc/aws/inventory.py
+++ libs/aws/odc/aws/inventory.py
@@ -1,14 +1,17 @@
-from concurrent.futures import ThreadPoolExecutor, as_completed
-from types import SimpleNamespace
-from io import BytesIO
-from gzip import GzipFile
 import csv
 import json
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from gzip import GzipFile
+from io import BytesIO
+from types import SimpleNamespace
 
 from . import s3_client, s3_fetch, s3_ls_dir
 
 
-def find_latest_manifest(prefix, s3, **kw):
+def find_latest_manifest(prefix, s3, **kw) -> str:
+    """
+    Find latest manifest
+    """
     manifest_dirs = sorted(s3_ls_dir(prefix, s3=s3, **kw), reverse=True)
 
     for d in manifest_dirs:
@@ -18,8 +21,17 @@ def find_latest_manifest(prefix, s3, **kw):
                 return d + "manifest.json"
 
 
-def retrieve_manifest_files(key: str, s3, schema, **kw):
+def retrieve_manifest_files(key: str, s3, schema, **kw) -> SimpleNamespace:
+    """
+    Retrieve manifest file and return a namespace
 
+    namespace(
+        Bucket=<bucket_name>,
+        Key=<key_path>,
+        LastModifiedDate=<date>,
+        Size=<size>
+    )
+    """
     bb = s3_fetch(key, s3=s3, **kw)
     gz = GzipFile(fileobj=BytesIO(bb), mode="r")
     csv_rdr = csv.reader(l.decode("utf8") for l in gz)
@@ -28,10 +40,18 @@ def retrieve_manifest_files(key: str, s3, schema, **kw):
         yield rec
 
 
-def list_inventory(manifest, s3=None, prefix: str = '', suffix: str = '', contains: str = '', **kw):
-    """Returns a generator of inventory records
+def list_inventory(manifest, s3=None, prefix: str = '', suffix: str = '', contains: str = '', **kw) -> SimpleNamespace:
+    """
+    Returns a generator of inventory records
 
     manifest -- s3:// url to manifest.json or a folder in which case latest one is chosen.
+
+    :param manifest: (str)
+    :param s3: (aws client)
+    :param prefix: (str)
+    :param suffix: (str)
+    :param contains: (str)
+    :return: SimpleNamespace
     """
     s3 = s3 or s3_client()
 
@@ -68,8 +88,8 @@ def list_inventory(manifest, s3=None, prefix: str = '', suffix: str = '', contai
             for namespace in future.result():
                 key = namespace.Key
                 if (
-                    key.startswith(prefix) and
-                    key.endswith(suffix) and
-                    contains in key
+                        key.startswith(prefix) and
+                        key.endswith(suffix) and
+                        contains in key
                 ):
                     yield namespace
