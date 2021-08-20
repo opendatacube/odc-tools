12b2629db7b3863bc326f5661ec4d4840847afa2
Sai Ma <Sai.Ma@ga.gov.au>
2021-08-20 01:20:05 +0000

    Put the detail version there

--- libs/stats/odc/stats/model.py
+++ libs/stats/odc/stats/model.py
@@ -388,12 +388,12 @@ class Task:
         if self.paths(ext=ext).items() is not None:
             for band, path in self.paths(ext=ext).items():
                 dataset_assembler.note_measurement(band,
-                                                path,
-                                                pixels=output_dataset[band].values.reshape([self.geobox.shape[0], self.geobox.shape[1]]),
-                                                grid=GridSpec(shape=self.geobox.shape,
-                                                                transform=self.geobox.transform,
-                                                                crs=CRS.from_epsg(self.geobox.crs.to_epsg())),
-                                                nodata=self.product.nodata)
+                                                   path,
+                                                   pixels=output_dataset[band].values.reshape([self.geobox.shape[0], self.geobox.shape[1]]),
+                                                   grid=GridSpec(shape=self.geobox.shape,
+                                                                 transform=self.geobox.transform,
+                                                                 crs=CRS.from_epsg(self.geobox.crs.to_epsg())),
+                                                   nodata=self.product.nodata)
 
         return dataset_assembler
 
