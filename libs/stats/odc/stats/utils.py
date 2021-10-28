dd04aca1722f5c7faaa754db8f0198fce9f69a6c
Paul Haesler <linkjuggler@gmail.com>
2021-10-28 16:03:49 +1100

    Fix bands in gm_ls_bitmask

--- libs/stats/odc/stats/utils.py
+++ libs/stats/odc/stats/utils.py
@@ -43,15 +43,9 @@ def bin_generic(
 
 
 def bin_seasonal(
-    cells: Dict[Tuple[int, int], Cell], months: int, anchor: int, extract_single_season=False
+    cells: Dict[Tuple[int, int], Cell], months: int, anchor: int
 ) -> Dict[Tuple[str, int, int], List[CompressedDataset]]:
-    # mk_single_season_rules is different from mk_season_rules
-    # because the mk_season_rules will split the whole year to 2/3/4 seasons
-    # but mk_single_season_rules only extract a single season from the whole year
-    if extract_single_season:
-        binner = season_binner(mk_single_season_rules(months, anchor))
-    else:
-        binner = season_binner(mk_season_rules(months, anchor))
+    binner = season_binner(mk_season_rules(months, anchor))
 
     tasks = {}
     for tidx, cell in cells.items():
@@ -90,32 +84,6 @@ def bin_annual(
             tasks[temporal_k + tidx] = dss
 
     return tasks
-
-    
-def mk_single_season_rules(months: int, anchor: int) -> Dict[int, str]:
-    """
-    Construct rules for a each year single season summary
-    :param months: Length of season in months can be one of [1, 12]
-    :param anchor: Start month of the season [1, 12]
-    """
-    assert 1 <= months <= 12
-    assert 1 <= anchor <= 12
-
-    rules: Dict[int, str] = {}
-
-    start_month = anchor
-
-    # not loop as mk_season_rules(). because it only return
-    # single season task cache each run
-    for m in range(start_month, start_month + months):
-        if m > 12:
-            m = m - 12
-        if months == 12:
-            rules[m] = f"{start_month:02d}--P1Y"
-        else:
-            rules[m] = f"{start_month:02d}--P{months:d}M"
-
-    return rules
 
 
 def mk_season_rules(months: int, anchor: int) -> Dict[int, str]:
