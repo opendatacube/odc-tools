fc0e50dca5d964d517ea95d88e8b4afa951e2cc8
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-06-21 00:10:01 +0000

    add update-if-exists test

--- apps/dc_tools/tests/test_s3_to_dc.py
+++ apps/dc_tools/tests/test_s3_to_dc.py
@@ -36,3 +36,22 @@ def test_s3_to_dc_stac(aws_env):
     )
     assert result.exit_code == 0
     assert result.output == "Added 1 datasets and failed 0 datasets.\n"
+
+
+
+@pytest.mark.depends(on=['add_products'])
+def test_s3_to_dc_stac_update_if_exist(aws_env):
+    runner = CliRunner()
+    # This will fail if requester pays is enabled
+    result = runner.invoke(
+        cli,
+        [
+            "--no-sign-request",
+            "--stac",
+            "--update-if-exists"
+            "s3://sentinel-cogs/sentinel-s2-l2a-cogs/42/T/UM/2022/1/S2A_42TUM_20220102_0_L2A/*.json",
+            "s2_l2a",
+        ],
+    )
+    assert result.exit_code == 0
+    assert result.output == "Added 1 datasets and failed 0 datasets.\n"
