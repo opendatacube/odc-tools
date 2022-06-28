2dd21e4ca987454f3912d1a0cf39fdc69355f04e
pin.jin@ga.gov.au <pin.jin@ga.gov.au>
2022-06-28 00:51:31 +0000

    add test case for s3-to-dc single absolute s3 path uri

--- apps/dc_tools/tests/test_s3_to_dc.py
+++ apps/dc_tools/tests/test_s3_to_dc.py
@@ -39,6 +39,23 @@ def test_s3_to_dc_stac(aws_env):
 
 
 @pytest.mark.depends(on=['add_products'])
+def test_s3_to_dc_single_stac(aws_env):
+    runner = CliRunner()
+    # This will fail if requester pays is enabled
+    result = runner.invoke(
+        cli,
+        [
+            "--no-sign-request",
+            "--stac",
+            "s3://sentinel-cogs/sentinel-s2-l2a-cogs/42/T/UM/2022/1/S2B_42TUM_20220114_0_L2A/S2B_42TUM_20220114_0_L2A.json",
+            "s2_l2a",
+        ],
+    )
+    assert result.exit_code == 0
+    assert result.output == "Added 1 datasets and failed 0 datasets.\n"
+
+
+@pytest.mark.depends(on=['add_products'])
 def test_s3_to_dc_stac_update_if_exist(aws_env):
     runner = CliRunner()
     # This will fail if requester pays is enabled
