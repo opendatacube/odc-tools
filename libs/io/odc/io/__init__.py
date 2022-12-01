8051e25557399865917878e792ccb9b993c49bc3
Damien Ayers <damien@omad.net>
2022-12-01 13:10:01 +1100

    linting(fix)

--- libs/io/odc/io/__init__.py
+++ libs/io/odc/io/__init__.py
@@ -1,9 +1,10 @@
 """ Various file io helpers
 """
-from ._version import __version__
 from .tar import tar_doc_stream
 from .text import parse_mtl, parse_yaml, read_stdin_lines, slurp, slurp_lines
 from .timer import RateEstimator
+
+__version__ = "0.2.1"
 
 __all__ = (
     "parse_yaml",
