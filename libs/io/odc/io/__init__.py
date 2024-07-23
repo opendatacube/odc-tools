6fe1261b1e910854ae3346961584f6a840a182af
Paul Haesler <linkjuggler@gmail.com>
2024-07-23 16:17:48 +1000

    Satisfy pylint

--- libs/io/odc/io/__init__.py
+++ libs/io/odc/io/__init__.py
@@ -1,11 +1,11 @@
 """ Various file io helpers
 """
 
-from .tar import tar_doc_stream  # pylint: disable=W0460
-from .text import parse_mtl, parse_yaml, read_stdin_lines, slurp, slurp_lines  # pylint: disable=W0460
-from .timer import RateEstimator  # pylint: disable=W0460
+from .tar import tar_doc_stream  # pylint: disable=W0406
+from .text import parse_mtl, parse_yaml, read_stdin_lines, slurp, slurp_lines  # pylint: disable=W0406
+from .timer import RateEstimator  # pylint: disable=W0406
 
-from ._version import __version__  # pylint: disable=W0460
+from ._version import __version__  # pylint: disable=W0406
 
 __all__ = (
     "parse_yaml",
