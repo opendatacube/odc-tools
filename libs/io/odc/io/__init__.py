c9370b28c527467a041b97574c3045c21ad2a3bc
Paul Haesler <linkjuggler@gmail.com>
2024-07-23 16:21:18 +1000

    Satisfy pylint

--- libs/io/odc/io/__init__.py
+++ libs/io/odc/io/__init__.py
@@ -3,12 +3,12 @@
 
 from .tar import tar_doc_stream  # pylint: disable=W0406
 from .text import (
-    parse_mtl,
-    parse_yaml,
-    read_stdin_lines,
-    slurp,
-    slurp_lines
-)  # pylint: disable=W0406
+    parse_mtl,  # pylint: disable=W0406
+    parse_yaml,  # pylint: disable=W0406
+    read_stdin_lines,  # pylint: disable=W0406
+    slurp,  # pylint: disable=W0406
+    slurp_lines  # pylint: disable=W0406
+)
 from .timer import RateEstimator  # pylint: disable=W0406
 
 from ._version import __version__  # pylint: disable=W0406
