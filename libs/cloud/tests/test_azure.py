5ca669ca5d434feebf87ad79ec678c0ace0c0afa
Paul Haesler <linkjuggler@gmail.com>
2024-07-23 12:54:03 +1000

    xfail broken lib-cloud tests.

--- libs/cloud/tests/test_azure.py
+++ libs/cloud/tests/test_azure.py
@@ -5,7 +5,7 @@ import pytest
 from odc.azure import download_yamls, find_blobs
 
 
-@pytest.xfail(reason="azure tests are broken")
+@pytest.mark.xfail(reason="Libcloud azure tests are broken")
 def test_find_blobs():
     """Find blobs in a sample Azure account, this will fail if the blob store changes or is removed"""
 
@@ -23,7 +23,7 @@ def test_find_blobs():
     assert len(blob_names) == 1
 
 
-@pytest.xfail(reason="azure tests are broken")
+@pytest.mark.xfail(reason="Libcloud azure tests are broken")
 def test_download_yamls():
     """Test pass/fail arms of YAML download from Azure blobstore"""
 
